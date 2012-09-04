8ccabd020194127f3df4191c4cdfcb997755d8a8
Ulli Hafner <ullrich.hafner@gmail.com>
2012-09-04 09:42:18 +0200

    Update of build scripts.

--- clean.sh
+++ clean.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-rm -rf $HUDSON_HOME/plugins/analysis-*
-rm -rf $HUDSON_HOME/plugins/checkstyle*
-rm -rf $HUDSON_HOME/plugins/dry*
-rm -rf $HUDSON_HOME/plugins/findbugs*
-rm -rf $HUDSON_HOME/plugins/pmd*
-rm -rf $HUDSON_HOME/plugins/tasks*
-rm -rf $HUDSON_HOME/plugins/warnings*
+rm -rf $JENKINS_HOME/plugins/analysis-*
+rm -rf $JENKINS_HOME/plugins/checkstyle*
+rm -rf $JENKINS_HOME/plugins/dry*
+rm -rf $JENKINS_HOME/plugins/findbugs*
+rm -rf $JENKINS_HOME/plugins/pmd*
+rm -rf $JENKINS_HOME/plugins/tasks*
+rm -rf $JENKINS_HOME/plugins/warnings*
 
-mvn clean install
+mvn clean install || { echo "Build failed"; exit 1; }
 
 cd ../
-cp */target/*.hpi $HUDSON_HOME/plugins
-cp findbugs/plugin/target/*.hpi $HUDSON_HOME/plugins
+cp */target/*.hpi $JENKINS_HOME/plugins
+cp findbugs/plugin/target/*.hpi $JENKINS_HOME/plugins
 
-
+cd $JENKINS_HOME
+./go.sh
 
 
