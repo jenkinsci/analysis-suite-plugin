e6e4d739a0552b21579f50835a84bb2441ad3963
Ulli Hafner <ullrich.hafner@gmail.com>
2014-08-31 01:03:08 +0200

    [FIXED JENKINS-24436] Don't use AbstractItem.getUrl anymore when creating job links.

--- clean.sh
+++ clean.sh
@@ -10,7 +10,6 @@ rm -rf $JENKINS_HOME/plugins/warnings*
 
 mvn clean install || { echo "Build failed"; exit 1; }
 
-cd ../
 cp */target/*.hpi $JENKINS_HOME/plugins
 cp findbugs/plugin/target/*.hpi $JENKINS_HOME/plugins
 
