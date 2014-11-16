479368313b7a76b448f13dcf4cc9dce8fcd51a9d
Ulli Hafner <ullrich.hafner@gmail.com>
2014-11-16 22:45:57 +0100

    [FIXED JENKINS-25511] [JENKINS-17309] Reverted XML escaping of message.
    Revert of fix for JENKINS-17309. Previous solution was too aggressive, it makes sense to escape only messages from parsers that parse non-XML files. XML parsers should not escape entities at all.
    AbstractAnnotation never escapes entities now, only GCC parser escapes and invokes constructor with escaped message.

---
+++ compile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -rf $JENKINS_HOME/plugins/analysis-*
+rm -rf $JENKINS_HOME/plugins/checkstyle*
+rm -rf $JENKINS_HOME/plugins/dry*
+rm -rf $JENKINS_HOME/plugins/findbugs*
+rm -rf $JENKINS_HOME/plugins/pmd*
+rm -rf $JENKINS_HOME/plugins/tasks*
+rm -rf $JENKINS_HOME/plugins/warnings*
+
+mvn clean install || { echo "Build failed"; exit 1; }
+
+cp */target/*.hpi $JENKINS_HOME/plugins
+cp findbugs/plugin/target/*.hpi $JENKINS_HOME/plugins
+
+
+
