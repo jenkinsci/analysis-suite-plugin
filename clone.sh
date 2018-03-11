98754d12e47c3f456a0adf824ed17e940dee1d09
Ulli Hafner <ullrich.hafner@gmail.com>
2018-03-11 21:03:13 +0100

    Renamed modules correctly with suffix.

--- clone.sh
+++ clone.sh
@@ -12,7 +12,7 @@ MODEL_BRANCH="master"
 PLUGIN_BRANCH="3.0"
 
 git clone -b ${MODEL_BRANCH} ${PROTOCOL}${GIT_ORGANISATION}/analysis-model.git
-git clone -b ${PLUGIN_BRANCH} ${PROTOCOL}${GIT_ORGANISATION}/analysis-config-plugin.git
-git clone -b ${PLUGIN_BRANCH} ${PROTOCOL}${GIT_ORGANISATION}/analysis-pom-plugin.git
-git clone -b ${PLUGIN_BRANCH} ${PROTOCOL}${GIT_ORGANISATION}/analysis-core-plugin.git
-git clone -b ${PLUGIN_BRANCH} ${PROTOCOL}${GIT_ORGANISATION}/warnings-plugin.git
+git clone -b ${PLUGIN_BRANCH} ${PROTOCOL}${GIT_ORGANISATION}/analysis-config-plugin.git analysis-config
+git clone -b ${PLUGIN_BRANCH} ${PROTOCOL}${GIT_ORGANISATION}/analysis-pom-plugin.git analysis-pom
+git clone -b ${PLUGIN_BRANCH} ${PROTOCOL}${GIT_ORGANISATION}/analysis-core-plugin.git analysis-core
+git clone -b ${PLUGIN_BRANCH} ${PROTOCOL}${GIT_ORGANISATION}/warnings-plugin.git warnings-plugin
