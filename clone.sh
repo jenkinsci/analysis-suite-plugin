cc18dba194825700f3007beada319d83d4e8638c
Ulli Hafner <ullrich.hafner@gmail.com>
2018-03-11 21:12:35 +0100

    Fixed warnings path.

--- clone.sh
+++ clone.sh
@@ -15,4 +15,4 @@ git clone -b ${MODEL_BRANCH} ${PROTOCOL}${GIT_ORGANISATION}/analysis-model.git
 git clone -b ${PLUGIN_BRANCH} ${PROTOCOL}${GIT_ORGANISATION}/analysis-config-plugin.git analysis-config
 git clone -b ${PLUGIN_BRANCH} ${PROTOCOL}${GIT_ORGANISATION}/analysis-pom-plugin.git analysis-pom
 git clone -b ${PLUGIN_BRANCH} ${PROTOCOL}${GIT_ORGANISATION}/analysis-core-plugin.git analysis-core
-git clone -b ${PLUGIN_BRANCH} ${PROTOCOL}${GIT_ORGANISATION}/warnings-plugin.git warnings-plugin
+git clone -b ${PLUGIN_BRANCH} ${PROTOCOL}${GIT_ORGANISATION}/warnings-plugin.git warnings
