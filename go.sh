e2a394409f451b838cb5dcf989f89bcc16ec9168
Ulli Hafner <ullrich.hafner@gmail.com>
2018-03-11 14:14:54 +0100

    Updated compile scripts.

--- go.sh
+++ go.sh
@@ -2,16 +2,11 @@
 
 mvn install || { echo "Build failed"; exit 1; }
 
-rm -rf $JENKINS_HOME/plugins/analysis-*
-rm -rf $JENKINS_HOME/plugins/checkstyle*
-rm -rf $JENKINS_HOME/plugins/dry*
-rm -rf $JENKINS_HOME/plugins/findbugs*
-rm -rf $JENKINS_HOME/plugins/pmd*
-rm -rf $JENKINS_HOME/plugins/tasks*
+rm -rf $JENKINS_HOME/plugins/analysis-core*
 rm -rf $JENKINS_HOME/plugins/warnings*
 
-cp ../*/target/*.hpi $JENKINS_HOME/plugins
-cp ../findbugs*/plugin/target/*.hpi $JENKINS_HOME/plugins
+cp ../analysis-core*/target/*.hpi $JENKINS_HOME/plugins
+cp ../warnings*/target/*.hpi $JENKINS_HOME/plugins
 
 cd $JENKINS_HOME
 ./go.sh
