ba96b7864672646e3a675c4a3627fc95437f5dcf
Ulli Hafner <ullrich.hafner@gmail.com>
2018-09-05 10:10:28 +0200

    Added end-of-life message.

--- go.sh
+++ go.sh
@@ -2,13 +2,10 @@
 
 mvn install || { echo "Build failed"; exit 1; }
 
-rm -rf $JENKINS_HOME/plugins/analysis-*
 rm -rf $JENKINS_HOME/plugins/checkstyle*
 rm -rf $JENKINS_HOME/plugins/dry*
 rm -rf $JENKINS_HOME/plugins/findbugs*
 rm -rf $JENKINS_HOME/plugins/pmd*
-rm -rf $JENKINS_HOME/plugins/tasks*
-rm -rf $JENKINS_HOME/plugins/warnings*
 
 cp */target/*.hpi $JENKINS_HOME/plugins
 cp findbugs/plugin/target/*.hpi $JENKINS_HOME/plugins
